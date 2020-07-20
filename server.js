50088566b481e9172ce2c7730486274ae59b211a
lipskij <emil.lipskij@gmail.com>
2020-07-20 20:30:31 +0300

    add sqlite db for sign up

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
+const User = require("./user");
 const salt = 10;
 const port = 3000;
-
-const users = [];
 
 app.use(bodyParser.json());
 
@@ -14,32 +13,36 @@ app.get("/style.css", (req, res) => res.sendFile(`${__dirname}/style.css`));
 app.get("/script.js", (req, res) => res.sendFile(`${__dirname}/script.js`));
 
 app.post("/signup", (req, res) => {
-  const user = users.find((user) => user.username === req.body.username);
-  if (user) {
-    res.status(400).send("This user name already exist");
-  } else {
-    bcrypt.hash(req.body.password, salt, function (err, hash) {
-      users.push({
-        username: req.body.username,
-        email: req.body.email,
-        password: hash,
+  const user = User.findAll({
+    where: {
+      username: req.body.username,
+    },
+  }).then((users) => {
+    if (users.length > 0) {
+      res.status(400).send("This user name already exist");
+    } else {
+      bcrypt.hash(req.body.password, salt, function (err, hash) {
+        const newUser = User.build({
+          username: req.body.username,
+          email: req.body.email,
+          password: hash,
+        });
+        newUser.save().then(() => res.send());
       });
-      console.log(users);
-      res.send();
-    });
-  }
+    }
+  });
 });
 
 app.post("/login", (req, res) => {
-  const user = users.find((user) => user.username === req.body.username);
+  const user = db.find((user) => user.username === req.body.username);
   if (!user) {
-    res.status(401).json({username: "User not found"});
+    res.status(401).json({ username: "User not found" });
   } else {
     bcrypt.compare(req.body.password, user.password, function (err, result) {
       if (result === true) {
         res.send(user);
       } else {
-        res.status(401).json({password: "Invalid password"});
+        res.status(401).json({ password: "Invalid password" });
       }
     });
   }
